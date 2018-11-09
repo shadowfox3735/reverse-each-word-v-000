88d124d9b3ce9f0cdc74a36290e618471bc91605
shadowfox3735 <normanpan180@gmail.com>
2018-11-09 22:13:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(pharse)
-  
+  new_phrase = [ ]
+  string_to_array = phrase.split
+  string_to_array.collect do |word|
+    new_phrase << word.reverse
+  end
+  new_phrase.join(" ")
+end 
 end
